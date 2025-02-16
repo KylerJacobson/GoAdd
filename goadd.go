85cff367517f4a24754fc04e2af524141ea0a4c7
Kyler Jacobson <40446054+KylerJacobson@users.noreply.github.com>
2025-02-16 10:10:12 -0700

    add godoc

--- goadd.go
+++ goadd.go
@@ -1,5 +1,7 @@
 package goadd
 
+// Add takes 2 integers and returns the sum.
+// [Learn more about addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(num1, num2 int) int {
 	return num1 + num2
 }
